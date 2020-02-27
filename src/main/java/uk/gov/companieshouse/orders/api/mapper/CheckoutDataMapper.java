c9dc1585351832ad2c9553f372e38a1222ef6cc4
rjadhav <rjadhav@companieshouse.gov.uk>
2020-02-27 12:32:21 +0000

    Create ItemDTO to allow setting of details that Payments API expects

--- src/main/java/uk/gov/companieshouse/orders/api/mapper/CheckoutDataMapper.java
+++ src/main/java/uk/gov/companieshouse/orders/api/mapper/CheckoutDataMapper.java
@@ -4,7 +4,11 @@ import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import uk.gov.companieshouse.orders.api.dto.CheckoutDataDTO;
+import uk.gov.companieshouse.orders.api.dto.ItemDTO;
 import uk.gov.companieshouse.orders.api.model.CheckoutData;
+import uk.gov.companieshouse.orders.api.model.Item;
+import uk.gov.companieshouse.orders.api.model.ItemCosts;
+import uk.gov.companieshouse.orders.api.model.Links;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +17,38 @@ import java.util.List;
 public interface CheckoutDataMapper {
 
     static final String CLASS_OF_PAYMENT_ORDERABLE_ITEM = "orderable-item";
+    static final String AVAILABLE_PAYMENT_METHOD_CREDIT_CARD = "credit-card";
+    static final String AVAILABLE_PAYMENT_METHOD_ACCOUNT = "account";
+    static final String ITEM_RESOURCE_TYPE = "cost#cost";
+    static final String CHECKOUT_RESOURCE_TYPE = "payment-details#payment-details";
+    static final String PAYMENT_STATUS = "pending";
+    static final String LINK_TYPE_SELF_URL_PREFIX = "/basket/payment/";
+
     CheckoutData checkoutDataDTOToCheckoutData(CheckoutDataDTO checkoutDataDTO);
     CheckoutDataDTO checkoutDataToCheckoutDataDTO(CheckoutData checkoutData);
 
     @AfterMapping
-    default void updateDTOWithPaymentClass(@MappingTarget CheckoutDataDTO checkoutDataDTO) {
+    default void updateDTOWithPaymentClass(CheckoutData checkoutData, @MappingTarget CheckoutDataDTO checkoutDataDTO) {
+        Item item = checkoutData.getItems().get(0);
+        ItemDTO itemDTO = checkoutDataDTO.getItems().get(0);
+
         List<String> classOfPayment = new ArrayList<>();
         classOfPayment.add(CLASS_OF_PAYMENT_ORDERABLE_ITEM);
-        checkoutDataDTO.getItems().get(0).setClassOfPayment(classOfPayment);
+        itemDTO.setClassOfPayment(classOfPayment);
+
+        ItemCosts itemCosts = item.getItemCosts();
+        String totalCost = itemCosts.getTotalCost();
+        itemDTO.setAmount(totalCost);
+
+        List<String> availablePaymentMethods = new ArrayList<>();
+        availablePaymentMethods.add(AVAILABLE_PAYMENT_METHOD_ACCOUNT);
+        availablePaymentMethods.add(AVAILABLE_PAYMENT_METHOD_CREDIT_CARD);
+        itemDTO.setAvailablePaymentMethods(availablePaymentMethods);
+
+        itemDTO.setKind(ITEM_RESOURCE_TYPE);
+        itemDTO.setResourceKind(ITEM_RESOURCE_TYPE);
+
+        checkoutDataDTO.setKind(CHECKOUT_RESOURCE_TYPE);
+        checkoutDataDTO.setStatus("pending");
     }
 }
