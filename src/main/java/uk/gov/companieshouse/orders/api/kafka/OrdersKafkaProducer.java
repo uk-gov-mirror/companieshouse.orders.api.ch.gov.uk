7cd05a2d8a97c459884ba7cf1124c0240b4a0347
rjadhav <rjadhav@companieshouse.gov.uk>
2020-03-13 14:34:49 +0000

    Update kafka broker addresses missing message to be more informative

--- src/main/java/uk/gov/companieshouse/orders/api/kafka/OrdersKafkaProducer.java
+++ src/main/java/uk/gov/companieshouse/orders/api/kafka/OrdersKafkaProducer.java
@@ -40,7 +40,8 @@ public class OrdersKafkaProducer implements InitializingBean {
         if (brokerAddresses != null && !brokerAddresses.isEmpty()) {
             config.setBrokerAddresses(brokerAddresses.split(","));
         } else {
-            throw new ProducerConfigException("Broker addresses for kafka broker not supplied, use the environment variable KAFKA_BROKER_ADDR");
+            throw new ProducerConfigException("Broker addresses for kafka broker missing, check if environment variable KAFKA_BROKER_ADDR is configured. " +
+                    "Hint: this may mean 'spring.embedded.kafka.brokers property' is not configured for integration tests.");
         }
 
         config.setRoundRobinPartitioner(true);
