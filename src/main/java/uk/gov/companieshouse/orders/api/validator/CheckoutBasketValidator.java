e9a9a1d9347606fe88f72c22b989f05ee3012f92
rjadhav <rjadhav@companieshouse.gov.uk>
2020-04-23 11:56:34 +0100

    Log exception for invalid item

--- src/main/java/uk/gov/companieshouse/orders/api/validator/CheckoutBasketValidator.java
+++ src/main/java/uk/gov/companieshouse/orders/api/validator/CheckoutBasketValidator.java
@@ -1,6 +1,8 @@
 package uk.gov.companieshouse.orders.api.validator;
 
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
 import uk.gov.companieshouse.orders.api.exception.ErrorType;
 import uk.gov.companieshouse.orders.api.model.Basket;
 import uk.gov.companieshouse.orders.api.model.BasketItem;
@@ -9,8 +11,11 @@ import uk.gov.companieshouse.orders.api.service.ApiClientService;
 import java.util.ArrayList;
 import java.util.List;
 
+import static uk.gov.companieshouse.orders.api.OrdersApiApplication.APPLICATION_NAMESPACE;
+
 @Component
 public class CheckoutBasketValidator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(APPLICATION_NAMESPACE);
     private ApiClientService apiClientService;
 
     public List<String> getValidationErrors(final Basket basket) {
@@ -20,10 +25,12 @@ public class CheckoutBasketValidator {
             errors.add(ErrorType.BASKET_ITEMS_MISSING.value);
         }
         else {
+            String itemUri = "";
             try {
-                String itemUri = basketItems.get(0).getItemUri();
+                itemUri = basketItems.get(0).getItemUri();
                 apiClientService.getItem(itemUri);
             } catch (Exception exception) {
+                LOGGER.error("Failed to get item " + itemUri, exception);
                 errors.add(ErrorType.BASKET_ITEM_INVALID.value);
             }
         }
