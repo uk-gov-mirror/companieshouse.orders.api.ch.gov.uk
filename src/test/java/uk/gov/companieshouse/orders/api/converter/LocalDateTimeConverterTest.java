c2782fb6f9da8be1da4d6cacf93a0e6f094d1b99
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2020-05-26 15:13:49 +0100

    Amended variable names to be constants

--- src/test/java/uk/gov/companieshouse/orders/api/converter/LocalDateTimeConverterTest.java
+++ src/test/java/uk/gov/companieshouse/orders/api/converter/LocalDateTimeConverterTest.java
@@ -9,15 +9,15 @@ import org.junit.jupiter.api.Test;
 
 public class LocalDateTimeConverterTest {
     
-    private final String bstString = "2020-04-21T10:11:12.345";
-    private final String gmtString = "2020-01-21T10:11:12.345";
-    private final LocalDateTime gmt = LocalDateTime.parse(gmtString);
-    private final LocalDateTime bst = LocalDateTime.parse(bstString);
+    private final static String BST_STRING = "2020-04-21T10:11:12.345";
+    private final static String GMT_STRING = "2020-01-21T10:11:12.345";
+    private final static LocalDateTime GMT = LocalDateTime.parse(GMT_STRING);
+    private final static LocalDateTime BST = LocalDateTime.parse(BST_STRING);
 
     @Test
     @DisplayName("Test to convert a String containing a BST date to a LocalDateTime object")
     public void successfulConvertSummerTimeDateStringToLocalDateTime() {
-        LocalDateTime result = LocalDateTimeConverter.convertStringToLocalDateTime(bstString);
+        LocalDateTime result = LocalDateTimeConverter.convertStringToLocalDateTime(BST_STRING);
         assertEquals(2020, result.getYear());
         assertEquals(4, result.getMonthValue());
         assertEquals(21, result.getDayOfMonth());
@@ -29,7 +29,7 @@ public class LocalDateTimeConverterTest {
     @Test
     @DisplayName("Test to convert a String containing a GMT date to a LocalDateTime object")
     public void successfulConvertGMTDateStringToLocalDateTime() {
-        LocalDateTime result = LocalDateTimeConverter.convertStringToLocalDateTime(gmtString);
+        LocalDateTime result = LocalDateTimeConverter.convertStringToLocalDateTime(GMT_STRING);
         assertEquals(2020, result.getYear());
         assertEquals(1, result.getMonthValue());
         assertEquals(21, result.getDayOfMonth());
@@ -41,15 +41,15 @@ public class LocalDateTimeConverterTest {
     @Test
     @DisplayName("Test to convert a LocalDateTime object representing a BST date to a String")
     public void successfulConvertSummerTimeDateLocalDateTimeToString() {
-        String result = LocalDateTimeConverter.convertLocalDateTimeToString(bst);
-        assertEquals(bstString, result);
+        String result = LocalDateTimeConverter.convertLocalDateTimeToString(BST);
+        assertEquals(BST_STRING, result);
     }
    
     @Test
     @DisplayName("Test to convert a LocalDateTime object representing a GMT date to a String")
     public void successfulConvertGMTDateLocalDateTimeToString() {
-        String result = LocalDateTimeConverter.convertLocalDateTimeToString(gmt);
-        assertEquals(gmtString, result);
+        String result = LocalDateTimeConverter.convertLocalDateTimeToString(GMT);
+        assertEquals(GMT_STRING, result);
     }
 
     @Test
