90648e6ddbb3c2811f51c5f953d65913070c2330
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2020-05-28 10:40:28 +0100

    Amended formatting to replace tabs with spaces

--- src/main/java/uk/gov/companieshouse/orders/api/OrdersApiApplication.java
+++ src/main/java/uk/gov/companieshouse/orders/api/OrdersApiApplication.java
@@ -7,13 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrdersApiApplication {
 
-	public static final String APPLICATION_NAMESPACE = "orders.api.ch.gov.uk";
-	public static final String REQUEST_ID_HEADER_NAME = "X-Request-ID";
-	public static final String LOG_MESSAGE_DATA_KEY = "message";
+    public static final String APPLICATION_NAMESPACE = "orders.api.ch.gov.uk";
+    public static final String REQUEST_ID_HEADER_NAME = "X-Request-ID";
+    public static final String LOG_MESSAGE_DATA_KEY = "message";
 
-	public static void main(String[] args) {
-	    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		SpringApplication.run(OrdersApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(OrdersApiApplication.class, args);
+    }
 
 }
