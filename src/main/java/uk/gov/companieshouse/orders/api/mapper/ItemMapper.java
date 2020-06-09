889bcb5574315ba526798148c3e881137e922d98
rjadhav <rjadhav@companieshouse.gov.uk>
2020-06-09 08:15:00 +0100

    Remove unnecessary change.

--- src/main/java/uk/gov/companieshouse/orders/api/mapper/ItemMapper.java
+++ src/main/java/uk/gov/companieshouse/orders/api/mapper/ItemMapper.java
@@ -15,7 +15,6 @@ public interface ItemMapper {
 
     @AfterMapping
     default void setDescription(Item item, @MappingTarget BasketItemDTO basketItemDTO){
-        basketItemDTO.setId(item.getId());
         basketItemDTO.setDescription(item.getDescription());
         basketItemDTO.setDescriptionIdentifier(item.getDescriptionIdentifier());
         basketItemDTO.setDescriptionValues(item.getDescriptionValues());
