28e114528cfedc1e3758f29db23db9aefae81ad1
rjadhav <rjadhav@companieshouse.gov.uk>
2020-07-06 11:40:59 +0100

    Add delivery details to basket when clearing it

--- src/test/java/uk/gov/companieshouse/orders/api/repository/BasketRespositoryImplTest.java
+++ src/test/java/uk/gov/companieshouse/orders/api/repository/BasketRespositoryImplTest.java
@@ -5,15 +5,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import uk.gov.companieshouse.orders.api.model.Basket;
+import uk.gov.companieshouse.orders.api.model.BasketData;
+import uk.gov.companieshouse.orders.api.model.DeliveryDetails;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BasketRespositoryImplTest {
@@ -24,9 +28,22 @@ public class BasketRespositoryImplTest {
     @Mock
     MongoTemplate mongoTemplate;
 
+    @Mock
+    Basket basket;
+
+    @Mock
+    BasketData basketData;
+
+    @Mock
+    DeliveryDetails deliveryDetails;
+
     @Test
     public void clearBasketDataByIdVerifyFindAndModifyCalledOnce() {
+        when(mongoTemplate.findOne(any(), any())).thenReturn(basket);
+        when(basket.getData()).thenReturn(basketData);
+        when(basketData.getDeliveryDetails()).thenReturn(deliveryDetails);
         repositoryUnderTest.clearBasketDataById("ID");
+        verify(mongoTemplate, times(1)).findOne(any(Query.class), eq(Basket.class));
         verify(mongoTemplate, times(1)).findAndModify(any(Query.class), any(Update.class), eq(Basket.class));
     }
 
