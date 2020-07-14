6b9264ca860870e77a7877c443330ce27acc2f02
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-07-14 09:19:56 +0100

    GCI-1022: Add ApiClientService item not found integration tests. - These currently fail.

--- src/test/java/uk/gov/companieshouse/orders/api/service/ApiClientServiceIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/orders/api/service/ApiClientServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package uk.gov.companieshouse.orders.api.service;
 
 import org.junit.ClassRule;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +11,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.web.server.ResponseStatusException;
 import uk.gov.companieshouse.orders.api.client.Api;
 import uk.gov.companieshouse.orders.api.mapper.ApiToItemMapper;
 import uk.gov.companieshouse.orders.api.model.*;
 
-import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static uk.gov.companieshouse.orders.api.util.TestConstants.VALID_CERTIFICATE_URI;
 import static uk.gov.companieshouse.orders.api.util.TestConstants.VALID_CERTIFIED_COPY_URI;
 
@@ -25,6 +28,9 @@ import static uk.gov.companieshouse.orders.api.util.TestConstants.VALID_CERTIFIE
 @SpringBootTest
 @SpringJUnitConfig(ApiClientServiceIntegrationTest.Config.class)
 public class ApiClientServiceIntegrationTest {
+
+    private static final String UNKNOWN_CERTIFICATE_URI = "/orderable/certificates/CRT-000000-000000";
+    public static final String UNKNOWN_CERTIFIED_COPY_URI = "/orderable/certified-copies/CCD-000000-000000";
 
     @Configuration
     @ComponentScan(basePackageClasses = {ApiClientServiceIntegrationTest.class, ApiToItemMapper.class, Api.class})
@@ -77,6 +83,40 @@ public class ApiClientServiceIntegrationTest {
         // Then
         assertThat(item instanceof CertifiedCopy, is(true));
         assertThat(item.getItemOptions() instanceof CertifiedCopyItemOptions, is(true));
+    }
+
+    @Test
+    @DisplayName("getItem() throws a 400 Bad Request response exception for unknown certificate")
+    void throwsBadRequestExceptionForCertificateNotFound () {
+
+        // Given
+        ENVIRONMENT_VARIABLES.set("CHS_API_KEY", "MGQ1MGNlYmFkYzkxZTM2MzlkNGVmMzg4ZjgxMmEz");
+        ENVIRONMENT_VARIABLES.set("API_URL", "http://api.chs-dev.internal:4001"); // TODO GCI-1022 WireMock
+        ENVIRONMENT_VARIABLES.set("PAYMENTS_API_URL", "http://api.chs-dev.internal:4001"); // TODO GCI-1022 WireMock
+
+        // When and then
+        final ResponseStatusException exception =
+                Assertions.assertThrows(ResponseStatusException.class,
+                        () -> serviceUnderTest.getItem(UNKNOWN_CERTIFICATE_URI));
+        assertThat(exception.getStatus(), is(BAD_REQUEST));
+        // TODO GCI-1022 assertThat(exception.getReason(), is("Error getting company name for company number 00006400"));
+    }
+
+    @Test
+    @DisplayName("getItem() throws a 400 Bad Request response exception for unknown certified copy")
+    void throwsBadRequestExceptionForCertifiedCopyNotFound () {
+
+        // Given
+        ENVIRONMENT_VARIABLES.set("CHS_API_KEY", "MGQ1MGNlYmFkYzkxZTM2MzlkNGVmMzg4ZjgxMmEz");
+        ENVIRONMENT_VARIABLES.set("API_URL", "http://api.chs-dev.internal:4001"); // TODO GCI-1022 WireMock
+        ENVIRONMENT_VARIABLES.set("PAYMENTS_API_URL", "http://api.chs-dev.internal:4001"); // TODO GCI-1022 WireMock
+
+        // When and then
+        final ResponseStatusException exception =
+                Assertions.assertThrows(ResponseStatusException.class,
+                        () -> serviceUnderTest.getItem(UNKNOWN_CERTIFIED_COPY_URI));
+        assertThat(exception.getStatus(), is(BAD_REQUEST));
+        // TODO GCI-1022 assertThat(exception.getReason(), is("Error getting company name for company number 00006400"));
     }
 
 }
