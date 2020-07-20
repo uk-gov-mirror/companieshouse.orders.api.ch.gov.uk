0a08561f7781f109581eb430affb100a11021bd5
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-07-20 09:15:35 +0100

    GCI-1022: Outline a way to read checkout item options correctly from DB.

---
+++ src/main/java/uk/gov/companieshouse/orders/api/listener/MongoCheckoutListener.java
@@ -0,0 +1,71 @@
+package uk.gov.companieshouse.orders.api.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.AfterConvertEvent;
+import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.orders.api.model.CertificateItemOptions;
+import uk.gov.companieshouse.orders.api.model.CertifiedCopyItemOptions;
+import uk.gov.companieshouse.orders.api.model.Checkout;
+import uk.gov.companieshouse.orders.api.model.Item;
+import uk.gov.companieshouse.orders.api.model.ItemOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+import static java.util.Arrays.stream;
+
+@Component
+public class MongoCheckoutListener extends AbstractMongoEventListener<Checkout> {
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    private enum ItemType {
+        CERTIFICATE("item#certificate", CertificateItemOptions.class),
+        CERTIFIED_COPY("item#certified-copy", CertifiedCopyItemOptions.class);
+
+        ItemType(final String kind, final Class<? extends ItemOptions> optionsType) {
+            this.kind = kind;
+            this.optionsType = optionsType;
+        }
+
+        private String kind;
+        private Class<? extends ItemOptions> optionsType;
+    }
+
+    @Override
+    public void onAfterConvert(final AfterConvertEvent<Checkout> event) {
+
+        final List<Item> items = event.getSource().getData().getItems();
+        final Document checkoutDocument = event.getDocument();
+        for (int index = 0; index < items.size(); index++) {
+            try {
+                readItemOptions(index, items, checkoutDocument);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    // TODO GCI-1022 Tidy up
+    void readItemOptions(final int index, final List<Item> items, final Document checkoutDocument) throws IOException {
+        final Item item = items.get(index);
+        final Document itemDocument = ((List<Document>) checkoutDocument.get("data", Document.class).get("items")).get(index);
+        final Document optionsDocument = itemDocument.get("item_options", Document.class);
+        // TODO GCI-1022 Find out why this seems necessary for the integration case mapper.setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE);
+        final ItemOptions options = mapper.readValue(optionsDocument.toJson(), getType(item.getKind()).optionsType);
+        item.setItemOptions(options);
+    }
+
+    ItemType getType(final String kind) {
+        return stream(ItemType.values())
+                .filter(value -> value.kind.equals(kind))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("'" + kind + "' is not a known kind!"));
+    }
+
+}
