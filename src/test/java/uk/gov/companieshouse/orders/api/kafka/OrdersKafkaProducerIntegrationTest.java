2d04de6da77eb1f7649a602eb4ac414a18c17913
rjadhav <rjadhav@companieshouse.gov.uk>
2020-08-05 12:37:51 +0100

    cleanup test

--- src/test/java/uk/gov/companieshouse/orders/api/kafka/OrdersKafkaProducerIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/orders/api/kafka/OrdersKafkaProducerIntegrationTest.java
@@ -1,15 +1,10 @@
 package uk.gov.companieshouse.orders.api.kafka;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import uk.gov.companieshouse.kafka.exceptions.SerializationException;
@@ -17,10 +12,6 @@ import uk.gov.companieshouse.kafka.message.Message;
 import uk.gov.companieshouse.orders.OrderReceived;
 
 import java.util.concurrent.ExecutionException;
-
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 @DirtiesContext
