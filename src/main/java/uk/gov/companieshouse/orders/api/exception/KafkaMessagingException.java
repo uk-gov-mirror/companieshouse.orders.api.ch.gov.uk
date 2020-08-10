61dfd5af53241ff9e1209c446b30d02443e6ac8f
rjadhav <rjadhav@companieshouse.gov.uk>
2020-08-10 11:35:16 +0100

    Remove unused imports

--- src/main/java/uk/gov/companieshouse/orders/api/exception/KafkaMessagingException.java
+++ src/main/java/uk/gov/companieshouse/orders/api/exception/KafkaMessagingException.java
@@ -1,8 +1,5 @@
 package uk.gov.companieshouse.orders.api.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class KafkaMessagingException extends RuntimeException {
 
     public KafkaMessagingException(String message) {
